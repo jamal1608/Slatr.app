70dd2853c21d5bf8dd9e2cab5fbc6ab697920d6c
jamal1608 <jamal1608@users.noreply.github.com>
2026-07-28 09:26:54 -0600

    fix: add material-icons-extended dependency (resolves Unresolved reference: Download/AccountBalanceWallet)

--- app/src/main/java/com/tonespace/app/MainActivity.kt
+++ app/src/main/java/com/tonespace/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.tonespace.app.ui.navigation.ToneShareNavGraph
 import com.tonespace.app.ui.theme.ToneSpaceTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ToneSpaceTheme {
-                // TODO: add your Compose content here
+                ToneShareNavGraph()
             }
         }
     }
